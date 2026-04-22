b7d494fcdebb514a91d4eb11850c5fa8b51962ac
adrian <adrianrabang@adrians-MacBook-Pro.local>
2026-04-22 20:06:47 +0100

    fix tailwind pipeline

--- app/OneSignalInit.tsx
+++ app/OneSignalInit.tsx
@@ -1,51 +1,23 @@
-"use client";
+'use client'
 
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    OneSignalDeferred: any[];
-  }
-}
+import { useEffect } from 'react'
 
 export default function OneSignalInit() {
   useEffect(() => {
-    if (typeof window === "undefined") return;
+    if (typeof window === 'undefined') return
 
-    if (!window.OneSignalDeferred) {
-      window.OneSignalDeferred = [];
+    const initOneSignal = async () => {
+      const OneSignal = (window as any).OneSignal || []
+
+      OneSignal.push(async function () {
+        await OneSignal.init({
+          appId: '0783b302-cb5a-474a-9f28-79869c2c0e03',
+        })
+      })
     }
 
-    window.OneSignalDeferred.push(async function (OneSignal: any) {
-      await OneSignal.init({
-        appId: "0783b302-cb5a-474a-9f28-79869c2c0e03",
-      });
+    initOneSignal()
+  }, [])
 
-      console.log("OneSignal READY");
-
-      try {
-        const id = await OneSignal.User.PushSubscription.id;
-
-        console.log("OneSignal ID:", id);
-
-        if (!id) return;
-
-        await fetch("/api/save-onesignal", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            onesignal_id: id,
-          }),
-        });
-
-        console.log("Saved OneSignal ID to backend");
-      } catch (err) {
-        console.error("OneSignal error:", err);
-      }
-    });
-  }, []);
-
-  return null;
+  return null
 }
